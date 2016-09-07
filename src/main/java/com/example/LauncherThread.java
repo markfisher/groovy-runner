da61ff1a626e9acd80ba3aab1403a380ab82750c
Dave Syer <dsyer@pivotal.io>
2016-09-07 15:18:35 +0100

    Don't use RunCommand

--- src/main/java/com/example/LauncherThread.java
+++ src/main/java/com/example/LauncherThread.java
@@ -17,12 +17,13 @@
 package com.example;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.boot.cli.command.run.RunCommand;
+import org.springframework.boot.cli.app.SpringApplicationLauncher;
 import org.springframework.boot.cli.compiler.GroovyCompiler;
 import org.springframework.boot.cli.compiler.GroovyCompilerConfiguration;
 import org.springframework.boot.cli.compiler.GroovyCompilerScope;
@@ -45,11 +46,15 @@ public class LauncherThread extends Thread {
 
 	private GroovyCompiler compiler;
 
+	private RunThread runThread;
+
+	private Object monitor = new Object();
+
 	public LauncherThread(ClassLoader classLoader, int count, String source,
 			String... args) {
 		super("spring-launcher-" + count);
 		this.count = count;
-		this.sources = new String[] {source};
+		this.sources = new String[] { source };
 		this.args = args;
 		setContextClassLoader(classLoader);
 		setDaemon(true);
@@ -62,28 +67,40 @@ public class LauncherThread extends Thread {
 	@Override
 	public void run() {
 		launch();
+		close();
 	}
 
 	private void launch() {
-		try {
-			this.compiler = new GroovyCompiler(new LauncherConfiguration());
-			RunCommand command = new RunCommand();
-			command.run(getArgs());
-			command.stop();
-			result = Collections.singletonMap("status", "DONE");
+		synchronized (this.monitor) {
+			try {
+				this.compiler = new GroovyCompiler(new LauncherConfiguration());
+				Object[] compiledSources = compile();
+				// Run in new thread to ensure that the context classloader is setup
+				this.runThread = new RunThread(compiledSources);
+				this.runThread.start();
+				this.runThread.join();
+			}
+			catch (Exception e) {
+				throw new IllegalStateException(e);
+			}
 		}
-		catch (Exception e) {
-			throw new IllegalStateException(e);
+	}
+
+	public void close() {
+		synchronized (this.monitor) {
+			if (this.runThread != null) {
+				this.runThread.shutdown();
+				this.runThread = null;
+				result = Collections.singletonMap("status", "DONE");
+			}
 		}
 	}
 
 	private String[] getArgs() {
-		String[] args = new String[this.args.length + 4];
-		args[0] = this.sources[0];
-		System.arraycopy(this.args, 0, args, 1, this.args.length);
-		args[this.args.length + 1] = "--";
-		args[this.args.length + 2] = "--spring.config.name=scriptlet";
-		args[this.args.length + 3] = "--spring.jmx.default-domain=scriptlet." + count;
+		String[] args = new String[this.args.length + 2];
+		System.arraycopy(this.args, 0, args, 0, this.args.length);
+		args[this.args.length ] = "--spring.config.name=scriptlet";
+		args[this.args.length + 1] = "--spring.jmx.default-domain=scriptlet." + count;
 		return args;
 	}
 
@@ -96,6 +113,65 @@ public class LauncherThread extends Thread {
 		return compiledSources;
 	}
 
+	private class RunThread extends Thread {
+
+		private final Object monitor = new Object();
+
+		private final Object[] compiledSources;
+
+		private Object applicationContext;
+
+		/**
+		 * Create a new {@link RunThread} instance.
+		 * @param compiledSources the sources to launch
+		 */
+		RunThread(Object... compiledSources) {
+			super("runner-" + count);
+			this.compiledSources = compiledSources;
+			if (compiledSources.length != 0 && compiledSources[0] instanceof Class) {
+				setContextClassLoader(((Class<?>) compiledSources[0]).getClassLoader());
+			}
+			setDaemon(true);
+		}
+
+		@Override
+		public void run() {
+			synchronized (this.monitor) {
+				try {
+					this.applicationContext = new SpringApplicationLauncher(
+							getContextClassLoader()).launch(this.compiledSources,
+									getArgs());
+				}
+				catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+
+		/**
+		 * Shutdown the thread, closing any previously opened application context.
+		 */
+		public void shutdown() {
+			synchronized (this.monitor) {
+				if (this.applicationContext != null) {
+					try {
+						Method method = this.applicationContext.getClass()
+								.getMethod("close");
+						method.invoke(this.applicationContext);
+					}
+					catch (NoSuchMethodException ex) {
+						// Not an application context that we can close
+					}
+					catch (Exception ex) {
+						ex.printStackTrace();
+					}
+					finally {
+						this.applicationContext = null;
+					}
+				}
+			}
+		}
+	}
 }
 
 class LauncherConfiguration implements GroovyCompilerConfiguration {
